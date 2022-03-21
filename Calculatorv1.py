2faf818817c0de10504a5678cd5af6c19e240926
chrisdotz <96923618+chrisdotz@users.noreply.github.com>
2022-03-20 22:25:34 -0500

    Add files via upload

---
+++ Calculatorv1.py
@@ -0,0 +1,13 @@
+def cal(a,b,c):
+    if b == "+":
+        print(f"{a} + {c} = {a+c}")
+    elif b == "-":
+        print(f"{a} - {c} = {a-c}")
+    elif b == "*":
+        print(f"{a} * {c} = {a*c}")
+    elif b == "/":
+        print(f"{a} / {c} = {a/c}")
+    else:
+        print("Input Invalid")
+
+cal(6,"/",3)
